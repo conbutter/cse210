175995cc235735c2fdaa1067773085fdb35ca7f1
TheWebCon <81337375+thewebcon@users.noreply.github.com>
2025-01-09 13:19:50 -0700

    Added var & logic tree to Sandbox.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        int x = 9;
+
         Console.WriteLine("Hello Sandbox World!");
+
+        if (x == 9) {
+            Console.WriteLine("X is equal to 9.");}
+        else {
+            Console.WriteLine("X is NOT equal to 9.");
+        }
     }
 }
