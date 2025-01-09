41c13eff39c6376b08569f9ece37b4ba97de0d76
TheWebCon <81337375+thewebcon@users.noreply.github.com>
2025-01-09 13:21:03 -0700

    Added comments to Sandbox.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,10 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Integer & variable declaration
         int x = 9;
 
+        // Print to console
         Console.WriteLine("Hello Sandbox World!");
 
+        // If/then statement
         if (x == 9) {
             Console.WriteLine("X is equal to 9.");}
         else {
