38480ca2e90c2dcebe55e03033ae38c0fb4103da
TheWebCon <81337375+thewebcon@users.noreply.github.com>
2025-01-09 17:44:43 -0700

    Completed Prep 1 assignment.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Obtain name
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        // Return stylized name
+        Console.WriteLine();        // Spacer line
+        Console.WriteLine($"My name is {lname}, {fname} {lname}.");
     }
 }
