cca558e93b5129c8a3ded5b0fb9b9a0e1c94c3cc
conbutter <145612052+conbutter@users.noreply.github.com>
2025-01-16 15:48:52 -0700

    Completed Learning2 assignment.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Nintendo";
+        job1._startYear = 2003;
+        job1._endYear = 2017;
+
+        Job job2 = new Job();
+        job2._jobTitle = "UX Designer";
+        job2._company = "Arrowhead Game Studios";
+        job2._startYear = 2018;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Harmond Row";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
