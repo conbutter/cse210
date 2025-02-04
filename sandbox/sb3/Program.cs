703edd2d80ef1918cbe830eeada0b44bc1fd91e1
conbutter <145612052+conbutter@users.noreply.github.com>
2025-02-04 13:10:51 -0700

    Added new sandbox project.

---
+++ sandbox/sb3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Shapes Program!")
+
+        var rect = new Rectangle();
+        rect.Display();
+
+        rect = new Rectangle(100, 200);
+        rect.Display;
+    }
+}
---
+++ sandbox/sb3/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿public class Rectangle
+{
+    // Attribute delcarations
+    private double _width;
+    private double _height;
+
+    // Constructor declarations
+    public Rectangle()
+    {
+        _width = 0;
+        _height = 0;
+    }
+
+    public Rectangle(double width, double height)
+    {
+        _width = width;
+        _height = height;
+    }
+}
