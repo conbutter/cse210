c6f37600918fa40081fb300145fe35dca52130ae
conbutter <145612052+conbutter@users.noreply.github.com>
2025-02-04 13:20:19 -0700

    Completed new sandbox.

---
+++ sandbox/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Shapes Program!")
+
+        var rect = new Rectangle();
+        rect.Display();
+
+        rect = new Rectangle(100, 200);
+        rect.Display;
+    }
+}
