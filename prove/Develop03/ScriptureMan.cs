626e89a4e4872864d56804b7b305f88c3f03a6d7
conbutter <145612052+conbutter@users.noreply.github.com>
2025-02-12 12:42:43 -0700

    Updated comments.

--- prove/Develop03/ScriptureMan.cs
+++ prove/Develop03/ScriptureMan.cs
@@ -3,7 +3,7 @@ class ScriptureMan
     // Attribute Declaration
     private List<Scripture> _scriptures = new List<Scripture>();
 
-    // Module Declaration
+    // Constructor Declaration
     public ScriptureMan()
     {
         _scriptures.Add(new Scripture("Alma 26:26-27", "But behold, my beloved brethren, we came into the wilderness not with the intent to destroy our brethren, but with the intent that perhaps we might save some few of their souls.|Now when our hearts were depressed, and we were about to turn back, behold, the Lord comforted us, and said: Go amongst thy brethren, the Lamanites, and bear with patience thine afflictions, and I will give unto you success."));
@@ -11,6 +11,7 @@ class ScriptureMan
         _scriptures.Add(new Scripture("Matthew 7:19-21", "19 Every tree that bringeth not forth good fruit is hewn down, and cast into the fire.|Wherefore by their fruits ye shall know them.|Not every one that saith unto me, Lord, Lord, shall enter into the kingdom of heaven; but he that doeth the will of my Father which is in heaven."));
     }
 
+    // Module Declaration
     public Scripture chooseRandomScripture()
     {
         Random random = new Random();
