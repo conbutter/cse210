1fb010d1473b196a25e5b172820de53509a1bf9d
conbutter <145612052+conbutter@users.noreply.github.com>
2025-02-13 13:14:52 -0700

    Updated case formatting.

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -3,16 +3,16 @@ class Reference
     // Attribute Declaration
     private string _book;
     private int _chapter;
-    private int _verse_start;
-    private int _verse_end;
+    private int _verseStart;
+    private int _verseEnd;
 
     // Constructor Declaration
     public Reference( string book, int chapter, int verse_start, int verse_end )
     {
         _book = book;
         _chapter = chapter;
-        _verse_start = verse_start;
-        _verse_end = verse_end;
+        _verseStart = verse_start;
+        _verseEnd = verse_end;
     }
 
     // Module Declaration
@@ -20,11 +20,11 @@ class Reference
     {
         Console.Clear();
         Console.Write($"{_book} {_chapter}:");
-        if ( _verse_start == _verse_end )
+        if ( _verseStart == _verseEnd )
         {
-            Console.Write($"{_verse_start} ");
+            Console.Write($"{_verseStart} ");
         } else {
-            Console.Write($"{_verse_start}-{_verse_end} ");
+            Console.Write($"{_verseStart}-{_verseEnd} ");
         }
     }
 }
