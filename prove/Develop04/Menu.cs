80d1b879cdba25acac0f999939cc1815fbc1d46b
conbutter <145612052+conbutter@users.noreply.github.com>
2025-02-25 15:33:23 -0700

    Restructured program.

--- prove/Develop04/Menu.cs
+++ prove/Develop04/Menu.cs
@@ -4,50 +4,15 @@ class Menu
 {
     // Attribute declaration
     string _menuContents;
-    bool _menuRunning;
 
     // Constructor declaration
 
     public Menu()
     {
         _menuContents = "Activity Program Menu\n 1. Begin breathing activity\n 2. Begin reflection activity\n 3. Begin listing activity\n 4. Quit program\nPlease type the number of the option you wish to select. ";
-        _menuRunning = false;
     }
 
     // Module declaration
-    
-    public void StartMenuLoop()
-    {
-        _menuRunning = true;
-        while ( _menuRunning == true )
-        {
-            DisplayMenu();
-            string userInput = Console.ReadLine();
-            if (userInput == "1")
-            {
-                BreathingActivity breathingActivity = new BreathingActivity("Breathing", "Throughout this exercise, we'll walk you through breathing in and out. Let your mind relax, and focus on your breathing.");
-                breathingActivity.DisplayIntro();
-                breathingActivity.PromptDuration();
-                breathingActivity.ShowGetReady();
-                breathingActivity.DoActivity();
-                breathingActivity.DisplayWellDone();
-            } else if (userInput == "2")
-            {
-                // Start reflection activity
-            } else if (userInput == "3")
-            {
-                // Start listing activity
-            } else if (userInput == "4")
-            {
-                _menuRunning = false;
-                Console.WriteLine("Quitting program...");
-            } else
-            {
-                Console.Clear();
-                Console.WriteLine("[!] That is not an acceptable response. Please choose from the list of options below.\n");
-            }
-        }
-    }
 
     public void DisplayMenu()
     {
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -6,8 +6,42 @@ class Program
     {
         Console.Clear();
 
-        // Begin program loop
+        // Create class objects & variables
         Menu mainMenu = new Menu();
-        mainMenu.StartMenuLoop();
+        BreathingActivity breathingActivity = new BreathingActivity("Breathing", "Throughout this exercise, we'll walk you through breathing in and out. Let your mind relax, and focus on your breathing.");
+        bool programRunning = true;
+
+        // Main program loop
+        while ( programRunning == true )
+        {
+            mainMenu.DisplayMenu();
+            string userInput = Console.ReadLine();
+            if (userInput == "1")
+            {
+                breathingActivity.DisplayIntro();
+                breathingActivity.PromptDuration();
+                breathingActivity.ShowGetReady();
+                breathingActivity.DoActivity();
+                breathingActivity.DisplayWellDone();
+            }
+            else if (userInput == "2")
+            {
+                // Start reflection activity
+            }
+            else if (userInput == "3")
+            {
+                // Start listing activity
+            }
+            else if (userInput == "4")
+            {
+                programRunning = false;
+                Console.WriteLine("Quitting program...");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("[!] That is not an acceptable response. Please choose from the list of options below.\n");
+            }
+        }
     }
 }
