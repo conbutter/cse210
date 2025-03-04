9d95f62f6f4add3a444577c267f92a9f83056024
conbutter <145612052+conbutter@users.noreply.github.com>
2025-03-04 13:09:02 -0700

    Added clarifying comment

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -13,7 +13,7 @@ abstract class Shape
 
     // Module declaration
 
-    public abstract double GetArea();
+    public abstract double GetArea(); // Instead of "abstract", could also be "virtual" for default body
 
     public string GetShapeColor()
     {
