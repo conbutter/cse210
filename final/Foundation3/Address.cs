7b9144016aef6156459cbaa89d23bf1983c3fd81
conbutter <145612052+conbutter@users.noreply.github.com>
2025-03-18 14:14:14 -0600

    Added Address class per program specifications.

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Address
+{
+    // Attribute declaration
+    private string _streetAddress;
+    private string _city;
+    private string _state;
+    private string _country;
+
+    // Constructor declaration
+    public Address(string streetAddress, string city, string state, string country)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _state = state;
+        _country = country;
+    }
+
+    // Module declaration
+
+    public string GetAddress()
+    {
+        return $"{_streetAddress}, {_city}, {_state} {_country}";
+    }
+}
