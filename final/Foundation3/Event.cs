2687c811816d1207092d6677b31b7ea507962ed9
conbutter <145612052+conbutter@users.noreply.github.com>
2025-03-18 14:17:16 -0600

    Laid out specifications for program actions.

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -5,22 +5,30 @@ class Event
     // Attribute declaration
     private string _title;
     private string _desc;
-    private DateTime date;
-    private DateTime time;
-    private string address;
+    private DateTime _date;
+    private DateTime _time;
+    private Address _address;
 
     // Constructor declaration
+    public Event(string title, string desc, DateTime date, DateTime time, Address address)
+    {
+        _title = title;
+        _desc = desc;
+        this._date = date;
+        this._time = time;
+        _address = address;
+    }
 
     // Module declaration
     public void DisplayStandard()
     {
-        // Standard details - Lists the title, description, date, time, and address.
+        Console.WriteLine($"{_title}\n- {_desc}\n{_date} @ {_time} @ {_address.GetAddress()}");
     }
 
     public void DisplayFull()
     {
-        //  Full details - Lists all of the above, plus type of event and information
-        //                 specific to that event type.
+        //  Full details - Lists all of the above  (title, description, date, time, and address)
+        //                 plus type of event and information specific to that event type.
         //                 For lectures, this includes the speaker name and capacity.
         //                 For receptions this includes an email for RSVP.
         //                 For outdoor gatherings, this includes a statement of the weather.
