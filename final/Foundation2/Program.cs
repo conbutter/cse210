cf524948a61856fe7153359ea976deb600d9d66d
conbutter <145612052+conbutter@users.noreply.github.com>
2025-03-20 13:07:07 -0600

    Updated comments.

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -19,22 +19,16 @@ class Program
 
     static void CreateOrders()
     {
-        // Create customers w/ addresses
         Customer customer1 = new Customer("Harmond Row", "1975 Tomorrow Way", "Tampa", "FL", "USA");
         Customer customer2 = new Customer("Bob Hale", "593 London Parkway", "London", "NW1 6XE", "UK");
 
-        // Create products
         Product product1 = new Product("Zorbeez", 100, 7, 30);
         Product product2 = new Product("Lysol Wipes", 101, 5, 55);
         Product product3 = new Product("Nintendo Switch", 102, 300, 3);
 
-        // Create two orders
         Order order1 = new Order(customer1, new List<Product> { product1, product2 });
         _orders.Add(order1);
         Order order2 = new Order(customer2, new List<Product> { product1, product2, product3 });
         _orders.Add(order2);
     }
 }
-
-// Call the methods to get the packing label, the shipping label,
-// and the total price of the order, and display the results of these methods.
